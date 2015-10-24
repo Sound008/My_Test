d41b89886bd397ef69a7ce95efd4b02697ba4bac
Sound008 <18leij2@MOO.local>
2015-10-24 15:47:50 +0800

    upgrades

---
+++ app.js
@@ -0,0 +1,19 @@
+var HP = 100;
+var hits = 0;
+
+
+
+function Destroy() {
+	hits++
+	document.getElementById('hitpoints').innerHTML = "You took down " + hits + "/100 of his HP.";
+
+		if(hits = 100)
+			alert("Congrats! You have successfully defeated the evil Jefflord.")
+
+		if(hits = 150)
+			alert("Stop, he's already dead.")
+		if(hits = 200)
+			alert("Last. Warning. Stop. Now.")
+		if(hits = 201)
+			alert("Screw you.")
+};
