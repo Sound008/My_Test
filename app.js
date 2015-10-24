085123aef7c0c24f02df61a2ae4afc3f8c84caa5
Sound008 <18leij2@MOO.local>
2015-10-24 15:55:27 +0800

    enhanced the if commands

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ function Destroy() {
 	hits++
 	document.getElementById('hitpoints').innerHTML = "You took down " + hits + "/100 of his HP.";
 
-		if(hits = 100)
-			alert("Congrats! You have successfully defeated the evil Jefflord.")
+		if(hits === 100)
+			alert("Congrats! You have successfully defeated the evil Jefflord. Do not stop this page from creating additional dialogues.")
 
-		if(hits = 150)
+		if(hits === 150)
 			alert("Stop, he's already dead.")
-		if(hits = 200)
+		if(hits === 200)
 			alert("Last. Warning. Stop. Now.")
-		if(hits = 201)
+		if(hits === 201)
 			alert("Screw you.")
 };
