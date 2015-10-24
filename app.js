93df3b4cb08930d300a8fe2afcbebf385f3f6b66
Sound008 <18leij2@MOO.local>
2015-10-24 15:56:35 +0800

    Re-enhanced the if/else statements

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ function Destroy() {
 		if(hits === 100)
 			alert("Congrats! You have successfully defeated the evil Jefflord. Do not stop this page from creating additional dialogues.")
 
-		if(hits === 150)
+		else if(hits === 150)
 			alert("Stop, he's already dead.")
-		if(hits === 200)
+		else if(hits === 200)
 			alert("Last. Warning. Stop. Now.")
-		if(hits === 201)
+		else if(hits === 201)
 			alert("Screw you.")
 };
