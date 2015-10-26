e2d8ca6f7f67c4388b707ddddb6e94b2f762e718
Sound008 <18leij2@MOO.local>
2015-10-26 16:17:01 +0800

    changed hits++ to hits—

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var HP = 100;
-var hits = 0;
+var hits = 100;
 
 
 
 function Destroy() {
-	hits++
-	document.getElementById('hitpoints').innerHTML = "You took down " + hits + "/100 of his HP.";
+	hits--
+	document.getElementById('hitpoints').innerHTML = "HP Left: " + hits + ".";
 
-		if(hits === 100)
+		if(hits === 0)
 			alert("Congrats! You have successfully defeated the evil Jefflord. Do not stop this page from creating additional dialogues.")
 
-		else if(hits === 150)
+		else if(hits === -50)
 			alert("Stop, he's already dead.")
-		else if(hits === 200)
+		else if(hits === -200)
 			alert("Last. Warning. Stop. Now.")
-		else if(hits === 201)
+		else if(hits === -201)
 			alert("Screw you.")
 };
